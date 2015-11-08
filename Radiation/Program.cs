515f20c6c2e4ee8a6a2542800c6da3b5d4916284
Xcelled <j12@ketelaar.com>
2015-11-08 16:56:29 -0500

    Added radiation logo

--- Radiation/Program.cs
+++ Radiation/Program.cs
@@ -8,8 +8,63 @@ namespace Radiation
 {
 	class Program
 	{
+		private const string ByLine = "Radiation by Xcelled";
+        private const int RadiationSymbolWidth = 40;
+		private static readonly string[] RadiationSymbol =
+		{
+			@"             =+$HM####@H%;,",
+			@"          /H###############M$,",
+			@"          ,@################+",
+			@"           .H##############+",
+			@"             X############/",
+			@"              $##########/",
+			@"               %########/",
+			@"                /X/;;+X/",
+			@"",
+			@"                 -XHHX-",
+			@"                ,######,",
+			@"#############X  .M####M.  X#############",
+			@"##############-   -//-   -##############",
+			@"X##############%,      ,+##############X",
+			@"-##############X        X##############-",
+			@" %############%          %############%",
+			@"  %##########;            ;##########%",
+			@"   ;#######M=              =M#######;",
+			@"    .+M###@,                ,@###M+.",
+			@"       :XH.                  .HX:",
+		};
+
 		static void Main(string[] args)
 		{
+			Console.Title = "☢ Radiation ☢";
+            PrintLogo();
+			Console.ReadKey();
+		}
+
+		private static void PrintLogo()
+		{
+			var cf = Console.ForegroundColor;
+			var cb = Console.BackgroundColor;
+
+			Console.ForegroundColor = ConsoleColor.Yellow;
+			Console.BackgroundColor = ConsoleColor.Black;
+
+			Console.WriteLine();
+
+			var padding = new string(' ', (Console.WindowWidth - RadiationSymbolWidth) / 2);
+
+			foreach (var line in RadiationSymbol)
+			{
+				Console.Write(padding);
+				Console.WriteLine(line);
+			}
+
+			Console.WriteLine();
+			Console.WriteLine(ByLine.PadLeft((Console.WindowWidth - ByLine.Length) / 2 + ByLine.Length));
+			Console.WriteLine();
+
+			Console.ForegroundColor = cf;
+			Console.BackgroundColor = cb;
 		}
 	}
 }
