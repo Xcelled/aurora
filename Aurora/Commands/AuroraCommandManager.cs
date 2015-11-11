0147e60ccee903e6c6fb8f31a810102659210af1
Xcelled <j12@ketelaar.com>
2015-11-11 17:48:09 -0500

    Started implementing better commands

--- Aurora/Commands/AuroraCommandManager.cs
+++ Aurora/Commands/AuroraCommandManager.cs
@@ -3,27 +3,75 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Aura.Channel;
 using Aura.Channel.Network;
+using Aura.Channel.Network.Sending;
 using Aura.Channel.Util;
+using Aura.Channel.Util.Configuration.Files;
 using Aura.Channel.World.Entities;
 using Aura.Shared.Util;
+using ManyConsole;
 
 namespace Aurora.Commands
 {
 	internal class AuroraCommandManager : GmCommandManager
 	{
 		public GmCommandManager OriginalManager { get; }
+		public List<ConsoleCommand> Commands { get; }
+
+		private readonly ArgumentTokenizer _tokenizer = new ArgumentTokenizer();
+		private readonly CommandsConfFile _conf = ChannelServer.Instance.Conf.Commands;
 
 		public AuroraCommandManager(GmCommandManager original)
 		{
 			OriginalManager = original;
+
+			Commands = new List<ConsoleCommand>();
+			foreach (var cmd in ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(AuroraCommandManager)))
+			{
+				if (string.IsNullOrEmpty(cmd.Command))
+					Log.Error("Command {0} did not call IsCommand!", cmd.GetType().Name);
+				else
+					Commands.Add(cmd);
+			}
 		}
 
 		public override bool Process(ChannelClient client, Creature creature, string message)
 		{
-			Log.Info("Aurora Commander Invoked");
+			if (!Handle(client, creature, message))
+				return OriginalManager.Process(client, creature, message);
+			return true;
+		}
 
-			return OriginalManager.Process(client, creature, message);
+		private bool Handle(ChannelClient client, Creature creature, string message)
+		{
+			if (message.Length < 2)
+				return false;
+
+			var isTargedCommand = message.StartsWith(_conf.Prefix2);
+			var isSelfCommand = !isTargedCommand && message.StartsWith(_conf.Prefix.ToString());
+
+			if (!isSelfCommand && !isTargedCommand)
+				return false;
+
+			message = message.Substring(isTargedCommand ? _conf.Prefix2.Length : _conf.Prefix.ToString().Length);
+
+			var sender = creature;
+			var target = creature;
+			var args = _tokenizer.Tokenize(message);
+
+			if (isTargedCommand)
+			{
+				// Get target player
+				if (args.Count < 2 || (target = ChannelServer.Instance.World.GetPlayer(args[1])) == null)
+				{
+					Send.ServerMessage(creature, Localization.Get("Target not found."));
+					return true;
+				}
+
+				// Remove target name from the args
+				args.RemoveAt(1);
+			}
 		}
 	}
 }
---
+++ Aurora/Commands/Command.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Aura.Channel;
+using Aura.Channel.Network;
+using Aura.Channel.Network.Sending;
+using Aura.Channel.World.Entities;
+using Aura.Shared.Util;
+
+namespace Aurora.Commands
+{
+	internal interface ICommand
+	{
+		bool IsNameMatch(string name);
+		void Execute(ChannelClient client, Creature sender, Creature target, IEnumerable<string> args);
+	}
+
+	internal abstract class Command : ICommand
+	{
+		private readonly int _defaultSelfAuth;
+		private readonly int _defaultTargetAuth;
+		protected readonly Regex NamePatternRegex;
+		public string Name { get; }
+
+		public int SelfAuth => ChannelServer.Instance.Conf.Commands.GetAuth(Name, _defaultSelfAuth, _defaultTargetAuth).Auth;
+
+		public int TargertAuth
+			=> ChannelServer.Instance.Conf.Commands.GetAuth(Name, _defaultSelfAuth, _defaultTargetAuth).CharAuth;
+
+		public bool CanBeTargeted => TargertAuth > 0;
+
+		protected Command(string name, int selfAuth = 99, int targetAuth = 99, string namePattern = null)
+			: this(name, new Regex($"^{namePattern ?? name}$", RegexOptions.CultureInvariant | RegexOptions.IgnoreCase), selfAuth, targetAuth)
+		{
+		}
+
+		protected Command(string name, Regex namePattern, int selfAuth = 99, int targetAuth = 99)
+		{
+			Name = name;
+			_defaultSelfAuth = selfAuth;
+			_defaultTargetAuth = targetAuth;
+			NamePatternRegex = namePattern;
+		}
+
+		public virtual void Execute(ChannelClient client, Creature sender, Creature target, IEnumerable<string> args)
+		{
+			var isSelfCommand = sender == target;
+			EnsureAuth(sender, isSelfCommand);
+
+			if (isSelfCommand)
+				EnsureTargeted();
+		}
+
+		public bool IsNameMatch(string name)
+		{
+			return NamePatternRegex.IsMatch(name);
+		}
+
+		protected bool HasAuth(Creature invoker, bool isSelfCommand)
+		{
+			var req = isSelfCommand ? SelfAuth : TargertAuth;
+			return req < invoker.Client.Account?.Authority;
+		}
+
+		protected void EnsureAuth(Creature invoker, bool isSelfCommand)
+		{
+			if (!HasAuth(invoker, isSelfCommand))
+			{
+				throw new Exception(string.Format(Localization.Get("You're not authorized to use '{0}'."), Name));
+			}
+		}
+
+		protected void EnsureTargeted()
+		{
+			if (!CanBeTargeted)
+			{
+				throw new Exception(string.Format(Localization.Get("Command '{0}' cannot be used on another character."), Name));
+			}
+		}
+	}
+}
